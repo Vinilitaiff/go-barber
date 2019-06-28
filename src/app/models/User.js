3cc068717e5d10a719365c005ea2a1f149be96c1
Vinicius <vinilitaiff@gmail.com>
2019-06-28 16:52:12 -0400

    implementacao do user controllers e model de user

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,18 +1,30 @@
+import bcrypt from 'bcryptjs';
 import Sequelize, { Model } from 'sequelize';
 
 class User extends Model {
   static init(sequelize) {
+    // INIT CHAMADO AUTOMATICAMENTE PELO SEQUELIZE
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
         password_hash: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        // eslint-disable-next-line no-param-reassign
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.findAll({
-    name: 'Vinicius Vasconcelos',
-    email: 'vinicius_vcv@homail.com',
-    password_hash: '12312312312',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
