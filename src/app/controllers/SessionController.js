a2cd0134aa28e45f6cd50603a72ba5a031a77c4a
Vinicius <vinilitaiff@gmail.com>
2019-07-03 17:02:53 -0400

    Update de user e validacao de dados de entrada com o Yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,9 +1,20 @@
 import jtw from 'jsonwebtoken';
+import * as Yup from 'Yup';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na Validacao' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
